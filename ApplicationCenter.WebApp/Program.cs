cb21152675821cb8a6c2bdb7162e8d9fd8392309
Robin <robin.lohe@gmx.de>
2025-05-26 23:44:48 +0200

    Add Auth0 authentication and authorization support

--- ApplicationCenter.WebApp/Program.cs
+++ ApplicationCenter.WebApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using ApplicationCenter.WebApp.Components;
+using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using MudBlazor.Services;
 
@@ -10,6 +13,13 @@ builder.Services.AddMudServices();
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddAuth0WebAppAuthentication(options =>
+{
+    options.Domain = builder.Configuration["Auth0:Domain"] ?? throw new InvalidOperationException();
+    options.ClientId = builder.Configuration["Auth0:ClientId"] ?? throw new InvalidOperationException();
+    options.Scope = "openid profile email";
+});
 
 builder.Services.AddScoped<ApplicationService>();
 builder.Services.AddScoped<ConfigurationKeyService>();
@@ -36,10 +46,32 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAntiforgery();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapStaticAssets();
+
+app.MapGet("/login", async Task (HttpContext httpContext, string redirectUri) =>
+{
+    var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+        .WithRedirectUri(redirectUri)
+        .Build();
+
+    await httpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+});
+
+app.MapGet("/logout", async httpContext =>
+{
+    var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+        .WithRedirectUri("/")
+        .Build();
+
+    await httpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+    await httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+});
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
