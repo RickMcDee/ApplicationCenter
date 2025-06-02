d9c0e57a2b19ae10797e59421c63dbc7d8fc7f60
Robin <robin.lohe@gmx.de>
2025-06-02 22:25:47 +0200

    Added Migrations for Userdata

---
+++ ApplicationCenter.WebApp/Migrations/UserData/20250602193351_UserManagement.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApplicationCenter.WebApp.Migrations.UserData
+{
+    /// <inheritdoc />
+    public partial class UserManagement : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "userdata");
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                schema: "userdata",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    EMail = table.Column<string>(type: "text", nullable: false),
+                    ExternalId = table.Column<string>(type: "text", nullable: false),
+                    AuthentificationProvider = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    LastSeenAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User",
+                schema: "userdata");
+        }
+    }
+}
